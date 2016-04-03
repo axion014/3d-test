878e25c48d6a13434cd8c6a7144cc5615ae4551f
axion014 <axion014@gmail.com>
2016-04-03 16:21:25 +0900

    make main menu

--- src/titlescene.js
+++ src/titlescene.js
@@ -5,48 +5,74 @@ phina.define('fly.TitleScene', {
 
 	init: function(params) {
 		this.superInit(params);
+
+		var menu = {
+			title: {
+				x: 0, y: 0, sub: [
+					{type: 'label', value: 'flygame', x: this.gridX.center(), y: this.gridY.span(4), size: 64},
+					{type: 'label', value: 'Click start', x: this.gridX.center(), y: this.gridY.span(12), size: 24, link: 'main'},
+					{type: 'model', name: 'flyer', value: phina.asset.AssetManager.get('threejson', 'fighter').get(), x: 0, y: 1000, z: 0},
+					{type: 'model', value: phina.asset.AssetManager.get('threecubetex', 'skybox').get(), x: 0, y: 1000, z: 0},
+					{
+						type: 'model', value: new THREE.Mesh(new THREE.CircleGeometry(10000, 100), new THREE.MeshBasicMaterial({
+							map: phina.asset.AssetManager.get('threetexture', 'plane').get()
+						})), x: 0, y: 0, z: 0, init: function(model) {model.rotate(-Math.PI / 2, 0, 0);}
+					}
+				]
+			},
+			main: {
+				x: -500, y: -500, sub: [
+					{type: 'label', value: 'Main Menu', x: this.gridX.center(), y: this.gridY.span(4), size: 64}
+				]
+			}
+		}
+
 		var layer = phina.glfilter.GLFilterLayer({width: SCREEN_WIDTH, height: SCREEN_HEIGHT});
 		var threelayer = phina.display.ThreeLayer({width: SCREEN_WIDTH, height: SCREEN_HEIGHT});
-		var flyer = phina.asset.AssetManager.get('threejson', 'fighter').get();
-		var sky = phina.asset.AssetManager.get('threecubetex', 'skybox').get();
-		var plane = new THREE.Mesh(new THREE.PlaneGeometry(10000, 10000), new THREE.MeshBasicMaterial({map: phina.asset.AssetManager.get('threetexture', 'plane').get()}));
-		flyer.position.y = 1000;
+
+		var group = phina.display.CanvasElement();
+
 		var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 		directionalLight.position.set(0, 0, 30);
-		sky.update = function() {this.move(flyer.position);};
-		plane.update = function() {
-			this.position.x = flyer.position.x;
-			this.position.z = flyer.position.z;
-		};
-		plane.rotate(-Math.PI / 2, 0, 0);
 		threelayer.scene.add(directionalLight);
 		threelayer.scene.add(new THREE.AmbientLight(0x606060));
-		threelayer.scene.add(flyer);
-		threelayer.scene.add(sky);
-		threelayer.scene.add(plane);
 		threelayer.update = function(app) { // Update routine
 			// Camera control
-			threelayer.camera.quaternion.copy(new THREE.Quaternion());
-			threelayer.camera.rotate(new THREE.Quaternion().setFromAxisAngle(Axis.x, Math.sin(this.frame * 0.01) * 0.25));
-			threelayer.camera.rotate(new THREE.Quaternion().setFromAxisAngle(Axis.y, Math.PI + this.frame * 0.005));
-			var vec = Axis.z.clone().applyQuaternion(threelayer.camera.quaternion).negate().setLength(-100);
-			threelayer.camera.position.copy(flyer.position.clone().add(vec));
+			this.flyer.quaternion.copy(new THREE.Quaternion());
+			this.flyer.rotate(new THREE.Quaternion().setFromAxisAngle(Axis.x, Math.sin(this.frame * 0.01) * 0.25));
+			this.flyer.rotate(new THREE.Quaternion().setFromAxisAngle(Axis.y, -Math.PI / 2 + this.frame * 0.005));
+			threelayer.camera.position.copy(new THREE.Vector3(-group.x / 15, 1000 + group.y / 15, 100));
 			threelayer.camera.updateMatrixWorld();
 			this.frame++;
 		}.bind(this);
-		var title = phina.display.Label({
-			text: 'flygame', fill: 'hsla(0, 0%, 0%, 0.6)',
-			stroke: false, fontSize: 64,
-		}).setPosition(this.gridX.center(), this.gridY.span(4));
-		var message = phina.display.Label({
-			text: 'Click start', fill: 'hsla(0, 0%, 0%, 0.6)',
-			stroke: false, fontSize: 24,
-		}).setPosition(this.gridX.center(), this.gridY.span(12));
 		layer.addChildTo(this);
 		threelayer.addChildTo(layer);
-		title.addChildTo(layer);
-		message.addChildTo(layer);
-		this.on('pointstart', function() {
+		group.addChildTo(layer);
+		var moveTo = function(x, y) {
+			group.tweener.to({x: x, y: y}, 1000, 'easeInOutCubic').play();
+		}
+		menu.forIn(function(key, value) {
+			for(var j = 0; j < value.sub.length; j++) {
+				var selects = value.sub[j];
+				if (selects.type === 'label') {
+					var label = phina.display.Label({text: selects.value, fill: 'hsla(0, 0%, 0%, 0.6)',
+						stroke: false, fontSize: selects.size}).addChildTo(group).setPosition(selects.x - value.x, selects.y - value.y);
+					if (selects.link) {
+						label.setInteractive(true);
+						label.onpointstart = function() {
+							moveTo(value[this.menuselects.link]);
+						}
+					}
+				} else if (selects.type === 'model') {
+					threelayer.scene.add(selects.value);
+					selects.value.position.set(selects.x, selects.y, selects.z);
+					if(selects.init) {selects.init(selects.value);}
+					if(selects.name) {this[selects.name] = selects.value;}
+				}
+			}
+		}.bind(this));
+
+		var start = function(stage) {
 			this.startframe = 0;
 			layer.alphaNode = phina.glfilter.AlphaNode(layer.gl, {
 				width: layer.domElement.width, height: layer.domElement.height
@@ -62,7 +88,7 @@ phina.define('fly.TitleScene', {
 				if (this.startframe === 40) {
 					layer.headNode.connectTo(layer.destNode);
 					this.off('enterframe', setfilter);
-					this.exit();
+					this.exit({stage: stage});
 				} else {
 					this.startframe++;
 				}
@@ -72,6 +98,11 @@ phina.define('fly.TitleScene', {
 				.connectTo(layer.zoomBlurNode)
 				.connectTo(layer.alphaNode)
 				.connectTo(layer.destNode);
-		});
+		}
+		var moveToMain = function() {
+			moveTo(-500, -500);
+			this.off('pointstart', moveToMain);
+		}
+		this.on('pointstart', moveToMain);
 	}
 });
