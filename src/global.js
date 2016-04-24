7c4012d57ac5053f6ef8aea9ce40eb166820eeda
axion014 <axion014@gmail.com>
2016-04-24 12:52:32 +0900

    fix screen size

--- src/global.js
+++ src/global.js
@@ -1,7 +1,14 @@
-var SCREEN_WIDTH = 480;
-var SCREEN_CENTER_X = 240;
-var SCREEN_HEIGHT = 720;
-var SCREEN_CENTER_Y = 360;
+var yrate = 1.5;
+var scale = 0.9;
+if (window.innerHeight / window.innerWidth > yrate) {
+	var SCREEN_WIDTH = window.innerWidth * scale;
+	var SCREEN_HEIGHT = window.innerWidth * yrate * scale;
+} else {
+	var SCREEN_WIDTH = window.innerHeight / yrate * scale;
+	var SCREEN_HEIGHT = window.innerHeight * scale;
+}
+var SCREEN_CENTER_X = SCREEN_WIDTH / 2;
+var SCREEN_CENTER_Y = SCREEN_HEIGHT / 2;
 
 phina.display.DisplayScene.default.$extend({
 	width: SCREEN_WIDTH,
